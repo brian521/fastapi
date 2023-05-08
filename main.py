b8d35ddd5112504185ff7f0c52891ec847a44f51
brian521 <59503208+brian521@users.noreply.github.com>
2023-05-08 14:46:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("http://127.0.0.1:8000")
+@app.post("/")
 async def root(request: Request):
     data = await request.json()
     input = data['input']
