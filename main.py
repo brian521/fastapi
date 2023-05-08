afd323cea0efed439107255268899e22c8c681b4
brian521 <59503208+brian521@users.noreply.github.com>
2023-05-08 14:12:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ app.mount("/", StaticFiles(directory="public", html = True), name="static")
 print("mount ok")
 
 origins = [
-    "https://port-0-fastapi-687p2alhd9msb3.sel4.cloudtype.app/",
-    "http://localhost",
-    "http://127.0.0.1:8000"
-    "http://127.0.0.1:5500"
+    #"https://port-0-fastapi-687p2alhd9msb3.sel4.cloudtype.app/",
+    #"http://localhost",
+    #"http://127.0.0.1:8000"
+    #"http://127.0.0.1:5500",
+    "*"
 ]
 
 app.add_middleware(
