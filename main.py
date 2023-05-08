8d37eef802162e1e8bf7ac0a50c9168c09b2c6da
brian521 <59503208+brian521@users.noreply.github.com>
2023-05-08 14:09:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,17 @@ app.mount("/", StaticFiles(directory="public", html = True), name="static")
 print("mount ok")
 
 origins = [
-    #"https://port-0-fastapi-687p2alhd9msb3.sel4.cloudtype.app/",
-    #"http://localhost",
-    #"http://127.0.0.1:8000"
-    #"http://127.0.0.1:5500",
-    "*"
+    "https://port-0-fastapi-687p2alhd9msb3.sel4.cloudtype.app/",
+    "http://localhost",
+    "http://127.0.0.1:8000"
+    "http://127.0.0.1:5500"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_method=["*"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
